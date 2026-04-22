a0eaacc4b16f8474126dfe94bfc2d3b7bc22f782
codemk-begginer <metenousteve@gmail.com>
2026-04-22 20:04:08 +0100

    mise en place des fonctionalites utilisateurs

--- Task_management_api/tasks/serializers.py
+++ Task_management_api/tasks/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'completed', 'user', 'user_id', 'created_at', 'updated_at']
--- Task_management_api/tasks/views.py
+++ Task_management_api/tasks/views.py
@@ -1,7 +1,25 @@
 from rest_framework import viewsets
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, User
+from .serializers import TaskSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        user_id = self.request.data.get('user_id')
+        if user_id:
+            serializer.save(user_id=user_id)
+        else:
+            serializer.save()
+
+    def perform_update(self, serializer):
+        user_id = self.request.data.get('user_id')
+        if user_id:
+            serializer.save(user_id=user_id)
+        else:
+            serializer.save()
